bbf6f82d0b0aa53f808271299de7ce3f6693e749
Martin G <martin.gras95@gmail.com>
2018-04-29 22:01:08 +0100

    slick plugin added using jquery to create a carousel in the jumbotron

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    arrows:false,
+    autoplay:true,
+    autoplayspeed:4000,
+    fade:false,
+    draggable:false,
+    pauseOnFocus:false,
+    pauseOnHover:false
+
+  });
+});
