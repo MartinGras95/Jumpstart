20f504ebf85fa14bfa65e344c2e18faf73707498
Martin G <martin.gras95@gmail.com>
2018-04-30 19:47:45 +0100

    Hide nav bar when scrolled down added

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,5 @@
+/* Using slick for carousel effect */
+
 $(document).ready(function() {
   $('.carousel').slick({
     arrows:false,
@@ -9,4 +11,21 @@ $(document).ready(function() {
     pauseOnHover:false
 
   });
+});
+
+
+/* Hiding the nav bar when scrolling down */
+
+var lastScrollTop = 0;
+
+$(window).scroll(function(){
+  var scrollTop = $(this).scrollTop();
+  if (scrollTop - lastScrollTop > 50){
+    var navHeight = $('.navbar').css('height');
+    $('.navbar').animate({top: '-' + navHeight}, 150);
+    lastScrollTop = scrollTop;
+  } else if (lastScrollTop - scrollTop > 50) {
+    $('.navbar').animate({top: '0px'}, 150);
+    lastScrollTop = scrollTop;
+  }
 });
